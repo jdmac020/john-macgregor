89de3b9be5fbb9e0e012f19409d8d31752c210ab
jdmac020 <john.macgregor@gmail.com>
2022-03-08 13:28:24 -0500

    Update test name

--- john-macgregor/src/App.test.js
+++ john-macgregor/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders logo image', () => {
   render(<App />);
   const logoImage = screen.getByAltText('A solid black wizard holding a staff in one hand and summoning an object with the other above the name John MacGregor and the slogan "build with magic"');
   expect(logoImage).toBeInTheDocument();
