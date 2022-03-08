af1d6716743911039d782a9a399986634919bbe6
jdmac020 <john.macgregor@gmail.com>
2022-03-08 13:18:55 -0500

    Hell World Page

--- john-macgregor/src/App.js
+++ john-macgregor/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import logo from './images/BlackLogoNoBackground.png'
+
+const logoAlt = 'A solid black wizard holding a staff in one hand and summoning an object with the other above the name John MacGregor and the slogan "build with magic"';
+const year = new Date().getFullYear();
 
 function App() {
   return (
-    <div>Coming Soon</div>
+    <div className='app'>
+      <header>Coming Soon</header>
+      <div className='app-grid-container'>
+        <img src={logo} alt={logoAlt}/>
+        <h2>Hello World</h2>
+        <p>This will soon be the space to learn all about a certain software developer named John MacGregor.</p>
+        <p>Till then, just enjoy things.</p>
+      </div>
+      <footer>&copy; {year} Johnathan MacGregor</footer>
+    </div>
   );
 }
 
