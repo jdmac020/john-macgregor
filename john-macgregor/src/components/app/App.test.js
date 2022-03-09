950baf67bfb89ac4855e1f6f31dff22a545c9abb
jdmac020 <john.macgregor@gmail.com>
2022-03-09 10:25:59 -0500

    Update import statement

--- john-macgregor/src/components/app/App.test.js
+++ john-macgregor/src/components/app/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './app';
+import App from './app.js';
 import functions from './appFunctions';
 
 const year = new Date().getFullYear();
