b5f9d161a4c9371b19d4f675deed42e0282aed36
jdmac020 <john.macgregor@gmail.com>
2022-03-09 09:35:58 -0500

    Set dynamic bottom margin; update link and scrollbar colors

--- john-macgregor/src/components/app/App.js
+++ john-macgregor/src/components/app/App.js
@@ -13,9 +13,23 @@ function App() {
         <header>{headerText}</header>
         <main>
           <img src={logo} alt={logoAlt}/>
-          <h2>Hello World</h2>
-          <p>This will soon be the space to learn all about a certain software developer named John MacGregor.</p>
-          <p>Till then, just enjoy things.</p>
+          <h2>Welcome!</h2>
+          <div>
+            <p>This is the online home for John MacGregor, the software professional who resides in Ohio, USA.</p>
+            <p>The goal of this site is to provide:</p>
+            <ul>
+              <li>A short bio</li>
+              <li>Links to</li>
+              <ul>
+                <li>Writings</li>
+                <li>Social media</li>
+                <li>My public body of work</li>
+              </ul>
+              <li>And a living example of my skills</li>
+            </ul>
+            <p>My intention is to provide an easy way for you to learn about me and what I can bring to the table, without a lot of hassle.</p>
+            <p>If you have any questions after poking around, don't hesitate to reach out to me <a href='mailto:john.macgregor@john-macgregor.dev'>via my email</a>.</p>
+          </div>
         </main>
         <footer>&copy; {footerText}</footer>
         <div className='right-gutter'></div>
