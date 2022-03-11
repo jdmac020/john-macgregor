0799e97376e018c62121b5d2e4387e49bbfb919d
jdmac020 <john.macgregor@gmail.com>
2022-03-11 08:21:47 -0500

    Remove appFunction and tests

--- john-macgregor/src/components/home/home.test.js
+++ john-macgregor/src/components/home/home.test.js
@@ -1,8 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import Home from './home.js';
-import functions from '../app/appFunctions';
-
-const year = new Date().getFullYear();
 
 describe('component tests', () => {
 
@@ -11,18 +8,5 @@ describe('component tests', () => {
     const logoImage = screen.getByAltText('A solid black wizard holding a staff in one hand and summoning an object with the other above the name John MacGregor and the slogan "build with magic"');
     expect(logoImage).toBeInTheDocument();
     expect(logoImage.getAttribute('src')).toBe('BlackLogoNoBackground.png');
-  });
-})
-
-describe('appFunctions', () => {
-  test('get footer text', () => {
-    const text = functions.footerText();
-    expect(text.includes(year)).toBe(true);
-    expect(text.includes('Johnathan MacGregor')).toBe(true);
-  });
-
-  test('get header text', () => {
-    const text = functions.headerText();
-    expect(text.includes('Home')).toBe(true);
   });
 })
